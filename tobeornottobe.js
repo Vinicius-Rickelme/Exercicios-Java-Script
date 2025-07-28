6054398b7306c5496620d365e06d63fc3b0c7fcd
Vinicius-Rickelme <viniciusrick165@gmail.com>
2025-07-28 15:58:00 -0300

    Add files via upload

---
+++ tobeornottobe.js
@@ -0,0 +1,30 @@
+function esperado(valor1) {
+
+  return {
+
+    toBe: function(valor2) {
+
+      if (valor1 === valor2) {
+        return "{value: true}"
+      } else {
+        return "{error: Not Equal}"
+      }
+
+    },
+    
+    notToBe: function(valor2) {
+
+      if (valor1 !== valor2) {
+        return "{value: true}"
+      } else {
+        return "{error: Equal}"
+      }
+
+    }
+  };
+}
+
+console.log(esperado(5).toBe(5));
+console.log(esperado(5).toBe(null));
+console.log(esperado(5).notToBe(null));
+console.log(esperado("ola mundo").notToBe("ola mundo"));
