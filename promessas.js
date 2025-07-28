fd2bbc5458290f82e5ea1e0f4320a838ab1c0a50
Vinicius-Rickelme <viniciusrick165@gmail.com>
2025-07-28 16:58:49 -0300

    Delete promessas.js

--- promessas.js
+++
@@ -1,8 +0,0 @@
-var addTwoPromises = async function(promise1, promise2) {
-  let value1 = await promise1;  
-  let value2 = await promise2;  
-  return value1 + value2;          
-};
-
-addTwoPromises(Promise.resolve(1), Promise.resolve(1))
-  .then(console.log); 
