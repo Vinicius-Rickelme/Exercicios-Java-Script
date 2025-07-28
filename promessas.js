d78c3e6bbd1edac6552642b3621c9fb8c43f17e1
Vinicius-Rickelme <viniciusrick165@gmail.com>
2025-07-28 16:56:57 -0300

    Update promessas.js

--- promessas.js
+++ promessas.js
@@ -1,6 +1,6 @@
 var addTwoPromises = async function(promise1, promise2) {
-  const value1 = await promise1;  
-  const value2 = await promise2;  
+  let value1 = await promise1;  
+  let value2 = await promise2;  
   return value1 + value2;          
 };
 
